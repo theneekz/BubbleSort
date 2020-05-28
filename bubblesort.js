5642bd2314e8e2b9509f3d33c1915f77659c20f7
Niko Sward <nicholascsward@gmail.com>
2020-05-28 13:29:26 -0500

    added didSwap bool and fixed test specs to spy on swap calls

--- bubblesort.js
+++ bubblesort.js
@@ -5,13 +5,22 @@ function swap(array, index) {
 }
 
 function bubbleSort(array) {
-  if (array.length < 2) {
-    return array;
-  } else {
+  //bool to see if we had to swap
+  let didSwap = true;
+  while (didSwap) {
+    //this will only stay false if we go through the rest and
+    //never swap anything! Saying the array is sorted.
+    didSwap = false;
+    //on each loop, decrement array.length
     for (let j = array.length; j > 0; j--) {
+      //iterate each item of remaining array
       for (let i = 0; i < j; i++) {
+        //if this element is greater than the one to the right
         if (array[i] > array[i + 1]) {
+          //swap em
           swap(array, i);
+          //track that you swapped and repeat the while loop
+          didSwap = true;
         }
       }
     }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,29 +1,30 @@
 /*eslint-env jasmine */
 /*global bubbleSort swap*/
-// describe('A spy', function () {
-//   let spyObject = {};
-//   beforeAll(function () {
-//     spyObject = {
-//       swap: swap,
-//     };
 
-//     spyOn(spyObject, 'swap').and.callThrough();
-//   });
 describe('Bubble Sort', function () {
+  beforeEach(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
   it('handles an empty array', function () {
     expect(bubbleSort([])).toEqual([]);
+    expect(swap.calls.count()).toEqual(0);
   });
   it('handles an array of size 2', function () {
     expect(bubbleSort([1, 0])).toEqual([0, 1]);
+    expect(swap.calls.count()).toEqual(1);
   });
   it('handles any array', function () {
-    expect(bubbleSort([0, 2, 5])).toEqual([0, 2, 5]);
-    expect(bubbleSort([2, 5, 0])).toEqual([0, 2, 5]);
-    expect(bubbleSort([2, 6, 3, 2])).toEqual([2, 2, 3, 6]);
+    expect(bubbleSort([2, 3, 1])).toEqual([1, 2, 3]);
+    expect(swap.calls.count()).toEqual(2);
+    expect(bubbleSort([22, 11, 13, 33, 55, 88, 77, 100])).toEqual([
+      11,
+      13,
+      22,
+      33,
+      55,
+      77,
+      88,
+      100,
+    ]);
   });
-  // it('calls swap', function () {
-  //   bubbleSort([]);
-  //   expect(spyObject.swap).toHaveBeenCalled();
-  // });
-  //   });
 });
